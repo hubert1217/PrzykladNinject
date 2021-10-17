2378576b3d78da440b3e8e0016807df09cc313ea
hubert1217 <hubert.roj@gmail.com>
2021-10-17 15:48:17 +0200

    Utworzenie koszyka na zakupy

---
+++ PrzykladNinject/Models/Cart.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PrzykladNinject.Models
+{
+    public class Cart
+    {
+        private List<CartLine> lineCollection = new List<CartLine>();
+    }
+}
